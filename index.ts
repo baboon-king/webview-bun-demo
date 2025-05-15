975319859dc591e38e1d0d06f298c96e223fcd1a
BaboonKing <changfan.xu.up@gmail.com>
2025-05-15 14:58:22 +0800

    feat: display content of user .zshrc file in webview

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 import { Webview } from "webview-bun";
 
+// 遍历当前目录文件
+// 用户目录的 .zshrc 文件
+const fs = Bun.file("/Users/baboon/.zshrc");
+
 const html = `
 <html>
     <body>
         <h1>Hello from bun v${Bun.version} !</h1>
+        <h2>File content:</h2>
+        <pre>
+        ${await fs.text()}
     </body>
 </html>
 `;
